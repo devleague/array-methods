a3a850996b28d904830cce8677d4d0b2630f9c59
sgnl <rayrfarias@gmail.com>
2015-05-22 18:25:51 -1000

    add push method specs and work description

---
+++ arrayMethods.js
@@ -0,0 +1,8 @@
+/**
+ * Declare a new function to be exported named `appendToArray`
+ * This function has two parameters:
+ *   @param  {Array} array
+ *   @param  {Any}   value
+ *
+ * @return {Array}   Array with the value appended to the end
+ */
--- test/arrayMethods-spec.js
+++ test/arrayMethods-spec.js
@@ -10,3 +10,27 @@ chai.use(sinonChai);
 var spy;
 
 var methodList = ['push', 'pop', 'reverse', 'shift', 'sort', 'splice', 'unshift', 'concat', 'join', 'slice', 'toString', 'indexOf', 'lastIndexOf', 'toLocaleString'];
+
+describe('push()', function () {
+  var appendToArray = arrayMethods.appendToArray;
+
+  beforeEach(function () {
+    spy = sinon.spy(Array.prototype, 'push');
+  });
+
+  afterEach(function () {
+    Array.prototype.push.restore();
+  });
+
+  it('should have a function named `appendToArray`', function () {
+    expect(appendToArray).to.be.defined;
+    expect(appendToArray).to.be.a('function');
+  });
+
+  it('should append a new item to an array', function () {
+    var newValue = '4';
+    var genericNumberArray = [1, 2, 3];
+    appendToArray(genericNumberArray, newValue);
+    expect(spy.args[0][0]).to.equal('4');
+  });
+});

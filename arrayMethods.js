ed2cd6a2a89c73661841b31cff5d8d55fff88c2f
Jody Breaux <jbreaux0001@gmail.com>
2017-06-13 18:57:30 -1000

    devSolution

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,52 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+var nameArray = [];
+nameArray.push("Jody", "Breaux");
+console.log(nameArray);
+genericNumberArray.push(6, 7, 8, 9, 10);
+console.log(genericNumberArray);
+var popResult = colors.pop();
+console.log(popResult);
+scrambledWords.reverse();
+phoneNumber.reverse();
+var nextOrder = orderQueue.shift();
+mixedNums.sort();
+mixedWords.sort();
+var y = fruitCollection.splice(1,3);
+console.log(y);
+//"y" is an array and can't be pushed into an array.
+var empty = [];
+var notFruit = empty.concat(y);
+console.log(notFruit);
+gemBox.splice(5, 2);
+gemBox.splice(6, 5);
+console.log(gemBox);
+upToTen.splice(2, 0, 3, 4, 5, 6, 7, 8);
+console.log(upToTen);
+console.log(brownOnly);
+brownOnly.fill("brown");
+brownOnly.splice(5, 2);
+//Automatically mutates the old array but can return a 
+//number value if it's assigned a variable.
+var orderedValuesNewLength = orderedValues.unshift(1, 2, 3);
+var randomThingsArray = genericNumberArray.concat(colors);
+//medium salad & Burger
+for(var i=0; i<orderQueue.length; i++){
+  console.log(orderQueue[i]);
+}
+
+var customOrder = [{takeOut: 'Chicken'}, {takeOut: 'Banana'}];
+updatedOrders = orderQueue.concat(customOrder);
+
+var sentence = scrambledWords.join(" ");
+console.log(sentence);
+var myFruits = fruitCollection.join(" + ");
+console.log(myFruits);
+var favoriteFriends = friends.slice(1, 3);
+var owesMoney = friends.slice(3);
+var monthNameString = monthNames.toString();
+var favoriteColor = colors.indexOf('Red');
+var favoriteEvenNumber = mixedNums.indexOf(76);
+var lastNine = bulkNumbers.lastIndexOf(9);
+var lastFive = bulkNumbers.lastIndexOf(5);

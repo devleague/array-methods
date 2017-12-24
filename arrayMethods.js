c2a7a2f5b68ab1ec6517c9bc108b77296dfea912
Mika Dumont <mikakrantman@gmail.com>
2017-12-23 20:21:52 -0800

    problems 1-6

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,15 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+nameArray.push("mika", "dumont");
+
+genericNumberArray.push(6,7,8,9,10);
+
+var popResult = colors.pop();
+
+scrambledWords.reverse();
+
+phoneNumber.reverse();
+
+var nextOrder = orderQueue.shift();
